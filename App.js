1bbafb7fcd31b5e074c6427887a96c433aab8938
Alex Zerah <alex.zerah@gmail.com>
2023-09-12 17:06:19 +0200

    init

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+
 
 export default function App() {
+  
+  const [poke, setPoke] = useState();
+
+
+  const getPoke = () => {
+    const fetching = fetch('https://pokeapi.co/api/v2/pokemon/429')
+      .then((response) => response.json())
+       .then((json) => setPoke(<Text key={json.id}>{json.name}</Text>))
+  }
+  getPoke();
+
   return (
     <View style={styles.container}>
+      {poke? poke : null}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
